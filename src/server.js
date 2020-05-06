b1b3f5f30480876add2591af6d7021e168c5f5d8
Jay Johnson <magic976@gmail.com>
2020-05-05 21:56:28 -0500

    Revert "Add packages and settings for Heroku"
    This reverts commit e644cfc3febb009f150ca634c278a18db2742a14.

--- src/server.js
+++ src/server.js
@@ -1,10 +1,5 @@
 const { Server } = require('boardgame.io/server');
-import path from 'path';
-import Koa from 'koa';
-import serve from 'koa-static';
-import mount from 'koa-mount';
 const SkullKing = require('./game.js').SkullKing;
-
 const server = Server({
     games: [SkullKing],
 });
@@ -12,19 +7,4 @@ const server = Server({
 const lobbyConfig = {
     apiPort: 8001,
 }
-
-// Build path relative to the server.js file
-const frontEndAppBuildPath = path.resolve(__dirname, '../build');
-
-// Serve the build directory
-const static_pages = new Koa();
-static_pages.use(serve(frontEndAppBuildPath));
-server.app.use(mount('/', static_pages));
-server.run({ port: 8000, lobbyConfig }, () => {
-    server.app.use(
-        async (ctx, next) => await serve(frontEndAppBuildPath)(
-            Object.assign(ctx, { path: 'index.html' }),
-            next
-        )
-    )
-});
+server.run({ port: 8000, lobbyConfig });
